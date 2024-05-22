6e0dbd3ee21b7d613682de158e587334c5187e75
GustavoGuerato <guerato.gustavo@gmail.com>
2024-05-22 12:09:31 -0700

    adicao da possibilidade de ver a maior nota

--- ArrayVector.java
+++ ArrayVector.java
@@ -24,7 +24,30 @@ public class ArrayVector {
       Disciplina disciplina1 = new Disciplina();
       disciplina1.setDisciplina("logica de programação");
       disciplina1.setNota(notas1);
-
       aluno.getDisciplinas().add(disciplina1);
+
+      System.out.println("nome do aluno é " + aluno.getNome() + " inscrito no curso: " + aluno.getEscola());
+
+      for (Disciplina disc : aluno.getDisciplinas()){
+        System.out.println("---------disciplina do aluno--------");
+        System.out.println("Disciplina: " + disc.getDisciplina());
+        System.out.println("As notas Das disciplinas são: ");
+
+        double notaMax = 0.0;
+
+        for (int pos = 0;pos< disc.getNota().length;pos++){
+          System.out.println("Nota " + pos + "é igual" + disc.getNota()[pos]);
+          if (pos ==0){
+            notaMax = disc.getNota()[pos];
+          }else{
+            if (disc.getNota()[pos]>notaMax){
+              notaMax = disc.getNota()[pos];
+            }
+          }
+        }
+
+        System.out.println("A maior nota da " + disc.getDisciplina() + "com o valor de " + notaMax);
+
+      }
     }
 }
